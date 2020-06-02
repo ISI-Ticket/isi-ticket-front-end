18cfbe41af49ae352067958b74e4a8a309b0a073
Eduardo Ribeiro <eagr1999@gmail.com>
2020-06-02 11:18:02 +0100

    update

--- front_end/vendor/js/signup.js
+++ front_end/vendor/js/signup.js
@@ -1,4 +1,46 @@
-window.onload = setData();
+window.onload = () => {
+    setData();
+
+    validatorSignUp();
+
+    const registerForm = document.getElementById('signUpForm');
+
+    registerForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        validatorSignUp();
+    });
+};
+
+const validatorSignUp = () => {
+    let validator = new Validator(document.querySelector('form[name="signUpForm"]'), function (err, res) {
+        if (res) {
+            register();
+        }
+    }, {
+            rules: {
+                customMinLength: function (value, params) {
+                    return this.min(value.replace(/\s{2,}/g, ' ').length, params);
+                },
+                nif: function (value, params) {
+                    return this.between(value.replace(/\s{2,}/g, ' ').length, params);
+                }
+            },
+            messages: {
+                en: {
+                    customMinLength: {
+                        empty: 'Insira a sua palavra-passe',
+                        incorrect: 'Introdoziu menos de {0} carateres'
+                    },
+                    nif: {
+                        empty: 'Insira o seu nif',
+                        incorrect: 'Nif inválido'
+                    }
+                }
+
+            }
+        });
+}
 
 function setData(){
     document.getElementById("email").value = localStorage.getItem('email');
